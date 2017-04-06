756397e74c0cb71c11ccb418e1e3ac6cd77de449
tinyiko@mavoni.co.za <tin2yiko3>
2017-04-06 19:06:13 +0200

    added tripRequest (renamed from ridersRequest)

---
+++ api/tripRequest.js
@@ -0,0 +1,25 @@
+/**
+ * Created by tinyiko on 2017/04/03.
+ */
+var promise = require("bluebird");
+var s2 = require("s2geometry-node");
+
+var riders_hashset = "RIDERS_HSET";
+
+function logRiderLocation(lat,lon,rider_UUID,mobile_number){
+    var s2Latlong = new s2.S2LatLng(lat,lon);
+    var s2riderCellId = new s2.S2CellId(s2Latlong);
+    //var s2driverCellId = s2driverCell.id();
+
+    var driver_data = {
+        key: s2riderCellId.id(),
+        lat:lat,
+        lon:lon,
+        date_time: new Date(),
+        driver_uuid: rider_UUID,
+        driver_mobile: mobile_number,
+        //s2cell_id:s2driverCellId.id() //initialize to cell_id of earth
+    };
+
+
+}
