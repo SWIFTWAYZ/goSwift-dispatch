37dbaac53d06914bd5a5781e167ae310b7db8e81
tinyiko@mavoni.co.za <tin2yiko3>
2017-04-06 19:05:27 +0200

    added redis provider for dispatch

---
+++ redis/redisProvider.js
@@ -0,0 +1,59 @@
+/**
+ * Created by tinyiko on 2017/04/03.
+ */
+
+(function() {
+
+    var redis = require("ioredis");
+    var client = new redis();
+
+    var redisService,
+        driver_hashset,
+        riders_hashset,
+        driver_sortedset,
+        riders_sortedset
+
+    var redisService = {};
+
+    driver_hashset = "DRIVERS_HSET";
+    riders_hashset = "RIDERS_HSET";
+    driver_sortedset = "drivers:list";
+    riders_sortedset = "riders:list";
+
+    var EXPIRE_DRIVER_GPS = 3600; //60 minutes
+    var EXPIRE_PASSENGER_GPS = 600; //10 minutes
+
+    /**
+     * method to add drivers to hashset
+     * @param hset_name
+     * @param value
+     */
+    var hsetAdd = function (hset_name, value) {
+        client.sadd(hset_name, value);
+        //console.log("sadd.....ioredis->" + value + "to---"+hset_name);
+
+    }
+
+    /**
+     * retrieve keys from driver hashset
+     * @type {Array}
+     */
+    var keys = function(cb){
+        client.keys(driver_hashset, function (err, data) {
+            console.log("logging keys ->" + data);
+            cb(data);
+        });
+    }
+
+    //attach methods and variables to object and export
+    redisService.hsetAdd = hsetAdd;
+    redisService.keys = keys;
+
+    redisService.driver_hashset = driver_hashset;
+    redisService.riders_hashset = riders_hashset;
+    redisService.driver_sortedset = driver_sortedset;
+    redisService.riders_sortedset = riders_sortedset;
+
+    exports.redisService = redisService;
+
+}).call(this);
